c41cd62f15733d9a6be2a688766ca462345ea93b
vmsysadm <55485877+vmsysadm@users.noreply.github.com>
2019-09-20 12:16:01 +0300

    Initial commit

---
+++ vmclone-init.ps1
@@ -0,0 +1,22 @@
+﻿Function vmclone-init {
+    param(
+		[Parameter(Mandatory=$true)]$template,
+		[Parameter(Mandatory=$true)]$user_data_file,
+        	[Parameter(Mandatory=$true)]$name
+    )
+	$vmhost =  get-vmhost | select -First 1
+	$vm = New-VM -Name $name -VM $template -VMHost $vmhost -DiskStorageFormat "thin"
+	
+	$ovf = Get-VMOvfProperty -VM (Get-VM -Name $name)
+	$ovfPropertyChanges = @{}
+	foreach ($obj in $ovf) {
+		$ovfPropertyChanges.add($obj.id,$obj.value)
+	}
+	
+	$ovfPropertyChanges."hostname" = $name
+	$ovfPropertyChanges."instance-id" = get-vm $name | %{(Get-View $_.Id).config.InstanceUuid}
+	$user_data = [Convert]::ToBase64String([IO.File]::ReadAllBytes($user_data_file))
+	$ovfPropertyChanges."user-data" = $user_data
+	$result = Set-VMOvfProperty -VM (Get-VM -Name $name) -ovfChanges $ovfPropertyChanges
+	Start-VM $name
+}
